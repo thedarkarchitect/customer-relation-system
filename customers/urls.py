1ce851726980e5ba2104126f7bfb2ec9f3833e4c
thedarkarchitect <romanreynolds240@gmail.com>
2023-03-27 15:09:32 +0300

    fourth commit

--- customers/urls.py
+++ customers/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     # path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
+    path('register/', views.register_user, name='register'),
 ]
--- customers/views.py
+++ customers/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import RegisterForm
 
 # Create your views here.
 def home(request):
@@ -26,3 +27,21 @@ def user_logout(request):
     logout(request)
     messages.success(request, "You have been logged out.")
     return redirect('home')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+
+            user = authenticate(username=username, password=password)
+            login(request, user)
+
+            messages.success(request, "You have successfully been registered")
+            return redirect('home')
+    else:
+        form = RegisterForm()
+        return render(request, 'register.html', {'form':form})
