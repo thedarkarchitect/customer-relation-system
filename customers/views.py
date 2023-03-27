c876e303cd25c8036febb1bc4cffb249e4ef9913
thedarkarchitect <romanreynolds240@gmail.com>
2023-03-27 18:26:52 +0300

    sixth commit

--- customers/views.py
+++ customers/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import RegisterForm
+from .models import Record
 
 # Create your views here.
 def home(request):
+    records = Record.objects.all()
+
     #check if user logged in 
     if request.method == 'POST':
         username = request.POST['username']
-        password = request.POST['password']
+        password = request.POST['password1']
         #user authentication
         user = authenticate(request, username=username, password=password)
         if user is not None:#this those an error of type none should be an httpresponse
@@ -19,8 +22,10 @@ def home(request):
             messages.success(request, "There was an error, Please Try Again...")
             redirect('home')
     else:
-        context ={}
+        context ={'records': records}
         return render(request, 'home.html', context)
+    
+    return render(request, 'home.html')
 
 
 def user_logout(request):
@@ -39,9 +44,9 @@ def register_user(request):
 
             user = authenticate(username=username, password=password)
             login(request, user)
-
             messages.success(request, "You have successfully been registered")
             return redirect('home')
     else:
         form = RegisterForm()
         return render(request, 'register.html', {'form':form})
+    return render(request, 'register.html', {'form':form})
